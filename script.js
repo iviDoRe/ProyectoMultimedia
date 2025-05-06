c22677e427eb9325b9a786f96fac0f6b2ab23107
iviDoRe <i.domingo2003@gmail.com>
2025-05-06 13:51:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let currentIndex = 0;
+
+const slides = document.querySelectorAll('.slider img');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  const slider = document.querySelector('.slider');
+  const offset = -index * 100;  // Mover la imagen hacia la izquierda
+  slider.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  showSlide(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+  showSlide(currentIndex);
+}
+
+// Inicia el slide automático
+setInterval(nextSlide, 10000); // Cambia la imagen cada 3 segundos
